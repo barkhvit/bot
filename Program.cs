072c0b66bf7b75e9a004535b5b50ecb7fecdbf70
Vitaly <barkhvit@gmail.com>
2025-03-17 19:26:26 +0300

    Homework-4 done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Bot
 {
@@ -8,15 +9,15 @@ namespace Bot
         public static string[] Input; //ввод от пользователя
         public static string[] CommandsBeforeStart = { "/start - запуск программы", 
             "/help - помощь", "/info - информация о программе", "/exit - выход" };//список команд, до команды Start.
-
         public static string[] CommandsAfterStart = { "/echo аргумент - вывод в консоль",
             "/help - помощь", "/info - информация о программе", "/exit - выход",
             "/addtask - добавить задачу", "/showtasks - показать список задач",
             "/removetask - удалить задачу","/clear - очистить экран"};//список команд, после команды Start.
-
+        public static int taskCountLimit;//лимит по кол-ву задач
+        public static int taskLengthLimit;//лимит по кол-ву символов в задаче
         public static List<string> Tasks = new List<string>(); // список задач
         public static bool ProgramIsStarting = false; // введена команда Start, программа запущена
-        
+
         static void Main(string[] args)
         {
             Console.WriteLine("Вас приветствует бот.");//приветствие
@@ -55,6 +56,27 @@ namespace Bot
                         default: WrongInput(); break;
                     }
                 }
+                catch(ArgumentException e) //некорректный аргумент при вводе кол-ва задач и символов в задаче или задача == null
+                {
+                    Console.WriteLine("Некорректный ввод.");
+                    Name = "";
+                    continue;
+                }
+                catch(TaskLengthLimitException e)//превышен лимит по символам в задаче
+                {
+                    Console.WriteLine($"{e.Message}\n");
+                    continue;
+                }
+                catch(DuplicateTaskException e)//если дубликат задачи
+                {
+                    Console.WriteLine($"{e.Message}\n");
+                    continue;
+                }
+                catch(TaskCountLimitException e)//превышен лимит кол-ва задач
+                {
+                    Console.WriteLine($"{e.Message}\n");
+                    continue;
+                }
                 catch(Exception e)
                 {
                     Console.WriteLine($"\nПроизошла непредвиденная ошибка: \n{e.GetType}\n{e.Message}\n{e.InnerException}\n{e.StackTrace}");
@@ -67,6 +89,7 @@ namespace Bot
             {
                 if (name == "") // без имени пользователя
                 {
+                    ProgramIsStarting = false;
                     Console.WriteLine("Доступные команды:");
                     PrintCommands(CommandsBeforeStart);
                 }
@@ -95,16 +118,17 @@ namespace Bot
             {
                 if (Name == "")
                 {
-                    Console.WriteLine("");
-                    Console.WriteLine("Введите Ваше имя:");
+                    Console.Write("\nВведите Ваше имя:");
                     Name = Console.ReadLine();
+                    Console.Write("Введите максимально допустимое количество задач:");
+                    taskCountLimit = ParseAndValidateInt(Console.ReadLine(), 1, 100);
+                    Console.Write("Введите максимально допустимую длину задачи:");
+                    taskLengthLimit = ParseAndValidateInt(Console.ReadLine(), 1, 100);
                     Console.Clear();
                 }
                 else
                 {
-                    Console.WriteLine("");
-                    Console.WriteLine("Программа уже запущена");
-                    Console.WriteLine("");
+                    Console.WriteLine("\nПрограмма уже запущена\n");
                 }
 
             }
@@ -122,8 +146,7 @@ namespace Bot
                 }
                 else
                 {
-                    Console.WriteLine(Input[1]);
-                    Console.WriteLine("");
+                    Console.WriteLine($"{Input[1]}\n");
                 }
                     
             }
@@ -131,55 +154,60 @@ namespace Bot
             //Команда /help
             void ComandHelp()
             {
-                Console.WriteLine("");
-                Console.WriteLine("Для старта программы введите команду: /start \nЕсли программа запущена то введите команду /echo и через пробел аргумент");
-                Console.WriteLine("");
+                Console.WriteLine("\nДля старта программы введите команду: /start \nЕсли программа запущена то введите команду /echo и через пробел аргумент\n");
             }
 
             //Команда /info
             void ComandInfo()
             {
-                Console.WriteLine("");
-                Console.WriteLine("| Версия программы: 1.0 | Дата создания: 12.03.2025 |");
-                Console.WriteLine("");
+                Console.WriteLine("\n| Версия программы: 1.0 | Дата создания: 12.03.2025 |\n");
             }
 
             //неверный ввод команды
             void WrongInput()
             {
-                Console.WriteLine("");
-                Console.WriteLine("Команда введена неверно");
-                Console.WriteLine("");
+                Console.WriteLine("\nКоманда введена неверно\n");
             }
 
             //неверный ввод команды Echo
             void WrongInputEcho()
             {
-                Console.WriteLine("");
-                Console.WriteLine("Ошибка ввода. После команды /echo через пробел необходимо ввести аргумент");
-                Console.WriteLine("");
+                Console.WriteLine("\nОшибка ввода. После команды /echo через пробел необходимо ввести аргумент\n");
             }
 
             //неверный ввод команды Echo
             void WrongInputAddtask()
             {
-                Console.WriteLine("");
-                Console.WriteLine("Для начала работы введите команду /start");
-                Console.WriteLine("");
+                Console.WriteLine("\nДля начала работы введите команду /start\n");
             }
 
             //команда добавить задачу
             void ComandAddTask()
             {
-                if (Name!="")
+                if (Name!="")//проверяем, что программа запущена
                 {
+                    //проверяем, что можно добавить задачу
+                    if (Tasks.Count >= taskCountLimit)
+                        throw new TaskCountLimitException(taskCountLimit);
+
                     Console.Write("Пожалуйста, введите описание задачи:");
                     string InputTask = Console.ReadLine();
+
+                    //проверяем задачу на корректность (длина, не null, не пустая)
+                    ValidateString(InputTask);
+
+                    //проверяем, что длина не превышает лимит
+                    if (InputTask.Length > taskLengthLimit)
+                        throw new TaskLengthLimitException(InputTask.Length, taskLengthLimit);
+
+                    //проверяем задачу на дубликат
+                    if(Tasks.Contains(InputTask))
+                        throw new DuplicateTaskException(InputTask);
+
                     if (InputTask != "")
                     {
                         Tasks.Add(InputTask);
-                        Console.WriteLine($"Задача \"{InputTask}\" добавлена.");
-                        Console.WriteLine();
+                        Console.WriteLine($"Задача \"{InputTask}\" добавлена.\n");
                     }
                 }
                 else
@@ -200,8 +228,7 @@ namespace Bot
                     }
                     else
                     {
-                        Console.WriteLine("Задач нет. Добавьте задачу с помощью команды /addtask");
-                        Console.WriteLine("");
+                        Console.WriteLine("Задач нет. Добавьте задачу с помощью команды /addtask\n");
                     }
                 }
                 else
@@ -241,20 +268,17 @@ namespace Bot
                             }
                             else
                             {
-                                Console.WriteLine("Некорректно введен номер задачи.");
-                                Console.WriteLine("");
+                                Console.WriteLine("Некорректно введен номер задачи.\n");
                             }
                         }
                         catch (Exception)
                         {
-                            Console.WriteLine("Некорректно введен номер задачи.");
-                            Console.WriteLine("");
+                            Console.WriteLine("Некорректно введен номер задачи.\n");
                         }
                     }
                     else
                     {
-                        Console.WriteLine("Задач нет. Добавьте задачу с помощью команды /addtask");
-                        Console.WriteLine("");
+                        Console.WriteLine("Задач нет. Добавьте задачу с помощью команды /addtask\n");
                     }
                 }
                 else
@@ -269,6 +293,29 @@ namespace Bot
                 Console.Clear();
                 Welcome(Name);
             }
+
+            //приводит строку к int и проверяет, что оно находится в диапазоне min и max
+            int ParseAndValidateInt(string? str, int min, int max)
+            {
+                if(int.TryParse(str, out int result))
+                {
+                    if (result <= max && result >= min)
+                        return result;
+                }
+                throw new ArgumentException();
+            }
+
+            //проверяет, что строка не равна null, не равна пустой строке и имеет какие-то символы кроме проблема
+            void ValidateString(string? str)
+            {
+                if (String.IsNullOrEmpty(str))//проверяем, что строка не ноль не пустая
+                    throw new ArgumentException();
+
+                int i = str.Replace(" ", "").Length;
+                if(i==0) throw new ArgumentException();//проверяем что строка не состоит только из пробелов
+            }
+
+            
         }
     }
 }
